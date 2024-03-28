92778f2b22368ec3d99542de614b79a030e461ee
SonaVarshney <sonavarshney29@gmail.com>
2024-03-28 19:55:26 +0530

    created express server and initialised basic routes

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import expressLayout from "express-ejs-layouts";
 
+import mainRoute from "./server/routes/main.js";
+
 const app = express(); //this creates express app or server
 dotenv.config();
 
 const PORT = 5000 || process.env.PORT;
 
-//Templating Engine
+app.use(express.static("public"));
 
-app.get("", (req, res) => {
-  res.send("Hello world");
-});
+//Templating Engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engines", "ejs");
+
+app.use("/main", mainRoute);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
